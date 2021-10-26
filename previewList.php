e1cfce4c347df42d9598023e4defecbca25581d3
Rahul <rahulchakraborty012@gmail.com>
2021-10-27 01:16:21 +0530

    issue fix

--- previewList.php
+++ previewList.php
@@ -1,46 +1,47 @@
 <?php
 require_once 'guide.php';
 if(!empty($_GET['guide_id'])){
-	$guideData = guideList($_GET['guide_id']);
-	$guideTitle = $guideDesc = $guideId ='';
-	if(!empty($guideData[0])){
-			$guideTitle = $guideData[0]['question_title'];
-			$guideDesc = $guideData[0]['question_description'];
-			$guideId = $guideData[0]['id'];
-	}
-	$guideoptionData = guideOptions($_GET['guide_id']);
+		$guideData = guideList($_GET['guide_id']);
+		$guideTitle = $guideDesc = $guideId ='';
+		if(!empty($guideData[0])){
+				$guideTitle = $guideData[0]['question_title'];
+				$guideDesc = $guideData[0]['question_description'];
+				$guideId = $guideData[0]['id'];
+		}
+		$guideoptionData = guideOptions($_GET['guide_id']);
 }
 $option_id="";
 $backLink='';
-$backLink1='';
+$backLink1='#';
 if(!empty($_GET['option_id'])){
-	$option_id=$_GET['option_id'];
-	$guideData = guideList('',$_GET['option_id']);
-	$guideTitle = $guideDesc = $guideId ='';
-	if(!empty($guideData[0])){
-		$guideTitle = $guideData[0]['question_title'];
-		$guideDesc = $guideData[0]['question_description'];
-		$guideId = $guideData[0]['id'];
-		$guideoptionData = guideOptions($guideId);			
-	}	
-   
-	$getPreviousData = getBackLink($_GET['option_id']);
-	if(!empty($getPreviousData[0])){
-		if(!empty($getPreviousData[0]['answer_option_id'])){
-			$backLink = "previewList.php?option_id=".$getPreviousData[0]['answer_option_id'];
+		$option_id=$_GET['option_id'];
+		$guideData = guideList('',$_GET['option_id']);
+		$guideTitle = $guideDesc = $guideId ='';
+		if(!empty($guideData[0])){
+			$guideTitle = $guideData[0]['question_title'];
+			$guideDesc = $guideData[0]['question_description'];
+			$guideId = $guideData[0]['id'];
+			$guideoptionData = guideOptions($guideId);			
+		}	
+	   
+		$getPreviousData = getBackLink($_GET['option_id']);
+		if(!empty($getPreviousData[0])){
+				if(!empty($getPreviousData[0]['answer_option_id'])){
+					$backLink = "previewList.php?option_id=".$getPreviousData[0]['answer_option_id'];
+				}
+				else {
+					$backLink = "previewList.php?guide_id=".$getPreviousData[0]['id'];
+				}
 		}
-		else{
-			$backLink = "previewList.php?guide_id=".$getPreviousData[0]['id'];
-		}
-	}
-	if(empty($guideoptionData)){
-		$guideId = getFirstGuideId($_GET['option_id']);
-		if(!empty($guideId){
-			$backLink1 = "previewList.php?guide_id=".$guideId;
-		}
-	}
-  	
+		if(empty($guideoptionData)){
+				$guideId = getFirstGuideId($_GET['option_id']);
+				if(!empty($guideId)){
+					$backLink1 = "preview_new.php?guide_id=".$guideId;
+				}
+		}  	
 }
+
+
 $content = '
     	<div class="row" style="margin-top:30px;">
     		<div class="col-md-4"></div>
